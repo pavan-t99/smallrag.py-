b339d2549dfddfc07ca646cf825274ba95f0725b
pavan-t99 <pavangjkpreings@gmail.com>
2026-03-17 09:31:42 +0000

    update2

--- app.py
+++ app.py
@@ -7,34 +7,38 @@ from Rag_pipeline import get_answer
 st.title("Ayushman Bharath Registration chatbot")
 st.write("Welcome to Ayushman bharath")
 #st.header("Header Example")
+if "selected" not in st.session_state:
+    st.session_state.selected = None
+
+if st.button("ENGLISH"):
+    st.session_state.selected = "ENGLISH"
+    st.write("ENGLISH SELECTED!")
+
+if st.button("Hindi"):
+    st.session_state.selected = "Hindi"
+    st.write("HINDI SELECTED!")
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
 
-english_answer=[]
-hindi_answer=[]
-selected=None
-if st.button("ENGILSH"):
-    selected="ENGILSH"
-    st.write("ENGILSH SELECTED!")
-if st.button("Hindi"):
-    st.write("HINDI SELECTED!")
-    selected="Hindi"
 user_input = st.chat_input("Ask your question")
 if user_input:
         st.session_state.messages.append({"role": "user", "content": user_input})
         recent_history=st.session_state.messages[-12:]
         with st.spinner("Searching Ayushman Bharat documents..."):
-            if selected=="Hindi":
-                hindi_answer[1]= get_answer(user_input,recent_history)
-                answer=hindi_answer[1]
-            #if selected=="ENGILSH":
-            english_answer[0]= get_answer(user_input,recent_history)
-            answer=english_answer[0]
-        st.session_state.messages.append({"role":"chat_bot","content":answer})
+            answers= get_answer(user_input,recent_history)
+            print(answers)
+            answer=answers[1]
+            if st.session_state.selected=="Hindi":
+                hindi_answer=answers[1]
+                answer=hindi_answer
+            if st.session_state.selected=="ENGILSH":
+                english_answer= answers[0].summary
+                answer=english_answer
+
+        st.session_state.messages.append({"role":"assistent","content":answer})
    
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
-        # st.chat_message(msg["reply"])
-        # st.write(msg["content_by_bot"])
+        
